2b4ec83ac6ccff7dbe7a3d758d068b1664c4cdc2
verc <u1benedict425@gmail.com>
2024-01-18 17:11:01 +0800

    changed navbar breakpoint

--- scripts/transparent_navbar.js
+++ scripts/transparent_navbar.js
@@ -1,7 +1,7 @@
 var navbar = document.getElementById("navbar");
 window.addEventListener("scroll", (event) => {
   let scroll = this.scrollY;
-  if (scroll > 100) {
+  if (scroll > 1) {
     navbar.classList.remove("transparent");
   } else {
     navbar.classList.add("transparent");
