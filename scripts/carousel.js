bacb6031238c97d0b470c127a868bb10af7eeaa4
verc <u1benedict425@gmail.com>
2024-01-29 12:06:53 +0800

    changed carousel behavior to be more dynamic

--- scripts/carousel.js
+++ scripts/carousel.js
@@ -1,48 +1,60 @@
 const images = document.getElementsByClassName("carousel-image");
-const nav = document.getElementsByClassName("carousel-dot");
+const carouselNav = document.getElementById("carousel-nav");
 const imagesContainer = document.getElementById("carousel-images");
 
-let slideIndex = 1;
+let slideIndex = 0;
 
 function plusSlides(n) {
-  showSlides((slideIndex += n));
+  var prev = slideIndex;
+  showSlides((slideIndex += n), prev);
 }
-function showSlides(n) {
-  let i;
-  if (n > images.length - 2) {
-    slideIndex = 1;
+function showSlides(n, prev) {
+  if (n > images.length - 1) {
+    slideIndex = 0;
   }
-  if (n < 1) {
-    slideIndex = images.length - 2;
+  if (n < 0) {
+    slideIndex = images.length - 1;
   }
-
-  var scrollWidth =
-    28 * parseFloat(getComputedStyle(document.documentElement).fontSize); //convert rem to pixels
-  imagesContainer.scroll(slideIndex * scrollWidth, 0);
+  console.log(
+    slideIndex,
+    slideIndex * (imagesContainer.scrollWidth / (images.length + 2)) -
+      prev * (imagesContainer.scrollWidth / (images.length + 2))
+  );
+  imagesContainer.scrollBy(
+    slideIndex * (imagesContainer.scrollWidth / (images.length + 2)) -
+      prev * (imagesContainer.scrollWidth / (images.length + 2)),
+    0
+  );
 }
-imagesContainer.addEventListener("scrollend", function () {
-  var scrollWidth =
-    28 * parseFloat(getComputedStyle(document.documentElement).fontSize);
-  var index = Math.round(imagesContainer.scrollLeft / scrollWidth);
-  console.log(index, images.length, imagesContainer.scrollLeft / scrollWidth);
-  if (index == 0) {
-    imagesContainer.scrollBy(scrollWidth, 0);
-  }
-  if (index == images.length - 1) {
-    imagesContainer.scrollBy(-scrollWidth, 0);
-  }
-  for (i = 0; i < nav.length; i++) {
-    nav[i].className = nav[i].className.replace(" active", "");
-  }
-  for (i = 0; i < images.length; i++) {
-    images[i].classList.remove("middle");
-  }
 
-  try {
-    images[index].classList.add("middle");
-    nav[index - 1].className += " active";
-  } catch (err) {
-    console.log("Carousel Error: ", err);
-  }
+const inMiddle = (entries, observer) => {
+  entries.forEach((entry) => {
+    entry.target.classList.toggle("is-inMiddle", entry.isIntersecting);
+    dots[parseInt(entry.target.dataset.index)].classList.toggle(
+      "active",
+      entry.isIntersecting
+    );
+    if (entry.isIntersecting && slideIndex != entry.target.dataset.index) {
+      slideIndex = parseInt(entry.target.dataset.index);
+    }
+  });
+};
+
+const carouselobsoptions = {
+  root: imagesContainer,
+  rootMargin: "0% -50% 0% -50%",
+  threshold: 0,
+};
+const CarouselObs = new IntersectionObserver(inMiddle, carouselobsoptions);
+
+document.querySelectorAll("[data-middle]").forEach((el) => {
+  CarouselObs.observe(el);
 });
-showSlides((slideIndex = 1));
+
+for (let i = 0; i < images.length; i++) {
+  var child = document.createElement("a");
+  child.className = "carousel-dot";
+  carouselNav.appendChild(child);
+}
+
+const dots = document.getElementsByClassName("carousel-dot");
